2e412e6695b01785a3a8210b9e5fc720b14ac8d0
vanessa-poirier <163433169+vanessa-poirier@users.noreply.github.com>
2025-11-26 17:38:47 -0500

    Update model_train.py

--- model_train.py
+++ model_train.py
@@ -1,1 +1,8 @@
+# THIS IS A SHORT AND SWEET SCRIPT FOR MODEL TRAINING
+# to be done after configuration.py
 model = train_model(model, train_loader, val_loader, criterion, optimizer, device) # function train model is defined in training_pipeline.py
+
+## EXAMPLE USE FOR SAVING
+root_dir = 'E:/dir'
+model_save_path = os.path.join(root_dir, "ResNet_results", "model_name.pth")
+torch.save(model.state_dict(), model_save_path) 
